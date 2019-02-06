95e76410417a38c811e5b747d0d1271d555d7bdb
Emanoel Barros <ebarros297@gmail.com>
2019-02-06 15:01:38 -0300

    create tweet action

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 
+import twitterLogo from '../twitter.svg';
+import './Login.css';
+
 export default class Login extends Component {
+    state = {
+        username: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({ username: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { username } = this.state;
+
+        if (!username) return;
+
+        localStorage.setItem('@GoTwitter:username', username);
+
+        this.props.history.push('/timeline');
+    }
+
     render() {
-        return <h1>Login</h1>;
+        return (
+            <div className="login-wrapper">
+                <img src={twitterLogo} alt="GoTwitter" />
+                <form onSubmit={this.handleSubmit}>
+                    <input value={this.state.username} onChange={this.handleChange} placeholder="Nome de usuário" type="text" />
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
+        );
     }
 }
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import Tweet from '../components/Tweet';
+import socket from 'socket.io-client';
 
-// import { Container } from './styles';
+import twitterLogo from '../twitter.svg';
+import './Timeline.css';
 
 export default class Timeline extends Component {
+    state = {
+        tweets: [],
+        newTweet: ''
+    }
+
+    async componentDidMount() {
+        this.subscribeToEvents();
+
+        const response = await api.get('tweet');
+        this.setState({ tweets: response.data });
+    }
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:8080');
+
+        io.on('tweet', data => {
+            this.setState({ tweets: [data, ... this.state.tweets] });
+        });
+
+        io.on('like', data => {
+            this.setState({
+                tweets: this.state.tweets.map(
+                    tweet => tweet._id === data._id ? data : tweet
+                )
+            });
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({ newTweet: e.target.value });
+    }
+
+    handleNewTweet = async (e) => {
+        if (e.keyCode !== 13) return;
+
+        const content = this.state.newTweet;
+        const author = localStorage.getItem('@GoTwitter:username');
+
+        await api.post('tweet', { content, author });
+
+        this.setState({ newTweet: '' });
+    }
+
     render() {
-        return <h1>Timeline</h1>;
+        return (
+            <div className="timeline-wrapper">
+                <img height={24} src={twitterLogo} alt="GoTwitter" />
+                <form action="">
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={this.handleChange}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontecendo?"
+                    />
+                </form>
+
+                <ul className="tweet-list">
+                    {this.state.tweets.map(tweet => (
+                        <Tweet key={tweet._id} tweet={tweet} />
+                    ))}
+                </ul>
+            </div>
+        );
     }
 }
